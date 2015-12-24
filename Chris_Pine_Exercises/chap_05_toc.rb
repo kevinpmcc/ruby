28fb94621c50c202a9c894a21c6a9a88a171017f
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2015-12-24 16:30:39 +0000

    TOC chap 5 added

---
+++ Chris_Pine_Exercises/chap_05_toc.rb
@@ -0,0 +1,12 @@
+#https://pine.fm/LearnToProgram/chap_05.html
+
+lineWidth = 52
+
+puts "Table of Contents".center lineWidth
+puts
+print "Chapter 1:   Numbers".ljust lineWidth
+puts "page 1".rjust lineWidth / 8
+print "Chapter 2:   Letters".ljust lineWidth
+puts "page 72".rjust lineWidth / 8
+print "Chapter 3:   Variables".ljust lineWidth
+puts "page 118".rjust lineWidth / 8
