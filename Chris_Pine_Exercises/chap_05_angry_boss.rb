70d9837a15f428e0fdb05fc46db6a1034b42adab
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2015-12-24 15:51:14 +0000

    added chap5

---
+++ Chris_Pine_Exercises/chap_05_angry_boss.rb
@@ -0,0 +1,9 @@
+#https://pine.fm/LearnToProgram/chap_05.html
+
+# ask what you want
+# take reply and save as variable
+# shout back the variable and say you're fired
+
+puts "YOU AGAIN, WHAT DO YOU WANT?"
+reply = gets.chomp.upcase
+puts "LITTLE BABY SAYS \"#{reply}\"???!!!! YOU\'RE FIRED"
