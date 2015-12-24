bcaf70506bcd5f0d5f217a62a8f7c5fd25eba98d
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2015-12-24 14:49:00 +0000

    added chap01 of chris pine

---
+++ Chris_Pine_Exercises/chap_01_calc.rb
@@ -0,0 +1,17 @@
+#https://pine.fm/LearnToProgram/chap_01.html
+
+# hours in a year
+puts 24 * 365
+
+#minutes in a decade
+puts 60 * 24 * 365 * 10
+
+#seconds old are you approx
+puts 60 * 60 * 24 * 365 * 33
+
+# chocolates hope to eat in life
+puts 1 * 365 * 90
+
+# if i am 1235million seconds old, how old am I?
+
+puts 1235000000 / (60 * 60 * 24 * 365)
