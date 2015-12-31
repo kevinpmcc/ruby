9056637b3ef25e7e7e749bc6ab5f76e6bc301398
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2015-12-31 13:56:31 +0000

    updated ex3

--- Learn_Ruby_The_Hard_Way_Exercises/ex3.rb
+++ Learn_Ruby_The_Hard_Way_Exercises/ex3.rb
@@ -1,26 +1,40 @@
 # http://learnrubythehardway.org/book/ex3.html
 
+# this prints out what's in between the inverted commas
 puts "I will now count my chickens:"
 
+# this prints out Hens and then the total of the {}
 puts "Hens #{25 + 30 / 6}"
+# this prints out roosters and then the total of the {}
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+# this prints out what's in between the inverted commas
 puts "Now I will count the eggs:"
 
+# this prints out the total of the calculations
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# this prints out everything between the inverted commas
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# this prints out the total of the calculation
 puts 3 + 2 < 5 - 7
 
+# this prints out the words and then the calculation
 puts "What is 3 + 2? #{3 + 2}"
+# this prints out the words and then the calculation
 puts "What is 5 - 7? #{5 - 7}"
 
+# this prints out what's in between the inverted commas
 puts "Oh, that why it's false."
 
+# this prints out what's in between the inverted commas
 puts "How about some more."
 
+# this prints out the words then true or false for what's in between the {}
 puts "Is it greater? #{5 > -2}"
+# this prints out the words then true or false for what's in between the {}
 puts "Is it greater or equal? #{5 >= -2}"
+# this prints out the words then true or false for what's in between the {}
 puts "Is it less or equal? #{5 <= -2}"
 
