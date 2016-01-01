124f259836162eda95b27f99830599ec7e2495b3
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-01-01 13:02:35 +0000

    updates to ruby the hard way

---
+++ Learn_Ruby_The_Hard_Way_Exercises/ex6.rb
@@ -0,0 +1,33 @@
+# http://learnrubythehardway.org/book/ex6.html
+
+# sets variable types_of_people to be equal to 10
+types_of_people = 10
+# sets variable x to string which contains variable types_of_people
+x = "There are #{types_of_people} types of people."
+# sets variable binary to string binary
+binary = "binary"
+# sets variable do_not to string don't 
+do_not = "don't"
+# sets variable y to string which contains variables binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print the variable x
+puts x
+# print the variable y
+puts y
+
+# print the string which contains variable x
+puts "I said: #{x}."
+# print the string which contains the variable y
+puts "I also said: '#{y}'."
+
+# sets variable hilarious to false
+hilarious = false
+joke_evaluation = "Isn't this joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
