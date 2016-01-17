e26cff8bd2926cf0bde4ace1c28f77916975e4d7
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-01-17 13:46:55 +0000

    add ex19

--- Learn_Ruby_The_Hard_Way_Exercises/ex17.rb
+++ Learn_Ruby_The_Hard_Way_Exercises/ex17.rb
@@ -1,19 +1,24 @@
 # http://learnrubythehardway.org/book/ex17.html
 
+# from the input command first thing is from_file and second thing is to_file
 from_file, to_file = ARGV
-
+# print to the screen the below screen using the two input variables
 puts "Copying from #{from_file} to #{to_file}"
 
-# we could do these two on on line, how?
+# we could do these two on on one line, how?
+# the in_file variable opens up the from_file
 in_file = open(from_file)
+# the indata variable reads the in_file
 indata = in_file.read
-
+# print out the string including the length of variable indata
 puts "The input file is #{indata.length} bytes long"
-
+# this prints the string including checking if the variable in to_file exists
 puts "Does the output file exist? #{File.exist?(to_file)}"
 puts "Ready, hit RETURN to continue, CTRL-C to abort."
+# takes the input
 $stdin.gets
 
+# the out_file variable is equal to opening the to_file variable file, 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
